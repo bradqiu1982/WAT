0fc8d9ee2bec474e2fd9dc096b6957ba0abbc90b
Brad Qiu <brad.qiu@finisar.com>
2019-06-11 00:30:02 +0800

    add the OGP data by configure file

--- WAT/Models/WuxiLogic/WATSampleXY.cs
+++ WAT/Models/WuxiLogic/WATSampleXY.cs
@@ -10,7 +10,38 @@ namespace WAT.Models
         public static List<WATSampleXY> GetSampleXYByCouponGroup(string coupongroup)
         {
             var ret = new List<WATSampleXY>();
+            var watcfg = WXCfg.GetSysCfg();
+            if (watcfg.ContainsKey("OGPCONNSTR") && watcfg.ContainsKey("OGPSQLSTR"))
+            {
+                try {
+                    var ogpconn = DBUtility.GetConnector(watcfg["OGPCONNSTR"]);
+                    var sql = watcfg["OGPSQLSTR"];
+                    sql = sql.Replace("<coupongroup>", coupongroup);
+
+                    var dbret = DBUtility.ExeSqlWithRes(ogpconn, sql);
+                    DBUtility.CloseConnector(ogpconn);
+
+                    foreach (var line in dbret)
+                    {
+                        var tempvm = new WATSampleXY();
+                        tempvm.CouponID = UT.O2S(line[0]);
+                        tempvm.ChannelInfo = UT.O2S(line[1]);
+                        tempvm.X = UT.O2S(line[2]);
+                        tempvm.Y = UT.O2S(line[3]);
+                        ret.Add(tempvm);
+                    }
+                } catch (Exception ex) { }
+            }
+
             return ret;
+        }
+
+        WATSampleXY()
+        {
+            CouponID = "";
+            ChannelInfo = "";
+            X = "";
+            Y = "";
         }
 
         public string CouponID { set; get; }
