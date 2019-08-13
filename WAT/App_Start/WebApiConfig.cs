6b532886b2bf22b19ba3d8189661c9b5b8233a5f
Brad Qiu <brad.qiu@finisar.com>
2019-08-13 09:17:01 +0800

    make web api can be query by action name

--- WAT/App_Start/WebApiConfig.cs
+++ WAT/App_Start/WebApiConfig.cs
@@ -16,6 +16,12 @@ namespace WAT
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Routes.MapHttpRoute(
+                name: "ActionApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
             config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
